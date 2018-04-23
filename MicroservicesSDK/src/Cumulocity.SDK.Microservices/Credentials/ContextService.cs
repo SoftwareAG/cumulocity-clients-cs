8f1ccd44da43f6ac415b66acf716ca820ac27a4e
IMPAQ\pnow <piotr.nowak@cumulocity.com>
2018-04-23 14:19:18 +0200

    Adding a context

---
+++ MicroservicesSDK/src/Cumulocity.SDK.Microservices/Credentials/ContextService.cs
@@ -0,0 +1,48 @@
+﻿using Cumulocity.SDK.Microservices.Settings;
+using Cumulocity.SDK.Microservices.Utils.Extenions;
+using Microsoft.AspNetCore.Http;
+
+namespace Cumulocity.SDK.Microservices.Credentials
+{
+	public interface IContextService
+	{
+		ICredentials GetCredentials();
+		string GetBaseUrl();
+	}
+
+	public class ContextService : IContextService
+	{
+		private readonly IHttpContextAccessor _contextAccessor;
+		private readonly Platform _platform;
+
+		public ContextService(IHttpContextAccessor ctx, Platform platform)
+		{
+			_contextAccessor = ctx;
+			_platform = platform;
+		}
+
+		public ICredentials GetCredentials()
+		{
+			if (_contextAccessor.HttpContext.User.IsInContext())
+			{
+				return new MicroserviceCredentialsBuilder()
+					.WithTenant(_contextAccessor.HttpContext.User.UserTenant())
+					.WithUsername(_contextAccessor.HttpContext.User.UserName())
+					.WithPassword(_contextAccessor.HttpContext.User.UserPassword())
+					.Build();
+			}
+
+			return new MicroserviceCredentialsBuilder()
+				.WithTenant(_platform.BOOTSTRAP_TENANT)
+				.WithUsername(_platform.BOOTSTRAP_USER)
+				.WithPassword(_platform.BOOTSTRAP_PASSWORD)
+				.Build();
+		}
+
+		public string GetBaseUrl()
+		{
+			return _platform.BASEURL;
+		}
+
+	}
+}
---
+++ MicroservicesSDK/src/Cumulocity.SDK.Microservices/Credentials/MicroserviceCredentials.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cumulocity.SDK.Microservices.Credentials
+{
+	public class MicroserviceCredentials : ICredentials
+	{
+		private string _tenant;
+		private string _username;
+		private string _password;
+		private string _tfaToken;
+		private string _appKey;
+		public string Username => _username;
+
+		public string Tenant => _tenant;
+
+		public string Password => _password;
+
+		public string TfaToken => _tfaToken;
+
+		public string AppKey => _appKey;
+
+		public MicroserviceCredentials(string tenant, string username, string password, string tfaToken, string appKey)
+		{
+			_tenant = tenant;
+			_username = username;
+			_password = password;
+			_tfaToken = tfaToken;
+			_appKey = appKey;
+		}
+	}
+}
