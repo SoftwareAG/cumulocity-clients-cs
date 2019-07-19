8228c7da3cb202ef20745dbd3a8c3eb6701d0410
IMPAQ\pnow <piotr.nowak@cumulocity.com>
2019-07-19 14:19:47 +0200

    MTM-24026 Demo OAuth

---
+++ Examples/MicroserviceSDK/MicroserviceExample/DemoOAuth/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DemoOAuth
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    var port = Environment.GetEnvironmentVariable("SERVER_PORT");
+
+                    if (int.TryParse(port, out var portNumber))
+                        options.Listen(IPAddress.Parse("0.0.0.0"), portNumber);
+                    else
+                    {
+                        options.Listen(IPAddress.Parse("0.0.0.0"), portNumber);
+                    }
+                })
+                .Build();
+        }
+    }
+}
