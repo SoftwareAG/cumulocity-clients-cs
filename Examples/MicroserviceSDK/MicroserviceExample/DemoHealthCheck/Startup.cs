cd34480d048697063ea312f7f1071be098e010bb
kumar_pallav_c8y <kumar.pallav@cumulocity.com>
2020-02-06 15:45:21 +0530

    Cumulocity-Client changes for netcoreapp3.1 upgradation

--- Examples/MicroserviceSDK/MicroserviceExample/DemoHealthCheck/Startup.cs
+++ Examples/MicroserviceSDK/MicroserviceExample/DemoHealthCheck/Startup.cs
@@ -53,11 +53,11 @@ namespace DemoHealthCheck
 			});
 
 			//MVC
-			services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+			services.AddRazorPages().AddMvcOptions(options => options.EnableEndpointRouting = false);
             services.Replace(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(TimedLogger<>)));
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseAuthentication();
             app.UseBasicAuthentication();
--- Examples/MicroserviceSDK/MicroserviceExample/DemoWebApi/Startup.cs
+++ Examples/MicroserviceSDK/MicroserviceExample/DemoWebApi/Startup.cs
@@ -48,7 +48,7 @@ namespace DemoWebApi
 			});
 
 			//MVC
-			services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+			services.AddRazorPages().AddMvcOptions(options => options.EnableEndpointRouting = false);
 			//services.Replace(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(TimedLogger<>)));
 		}
 		public virtual void ConfigureServicesLayer(IServiceCollection services)
@@ -57,7 +57,7 @@ namespace DemoWebApi
 			services.AddSingleton<IApplicationService, ApplicationService>();
 		}
 
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
 		{
 			app.UseAuthentication();
 			app.UseBasicAuthentication();
